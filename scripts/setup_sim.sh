be468e4dd176cd5379530e63884fad15b9886b25
mckeownish <j.mckeown124@gmail.com>
2024-04-22 22:17:56 +0100

    clearer output of each step to console

--- scripts/setup_sim.sh
+++ scripts/setup_sim.sh
@@ -11,20 +11,28 @@ source config.sh
 for pdbfile in ../pdbs/*.pdb; do
     basename=$(basename $pdbfile .pdb)
 
-    echo "--- * --- Dir Made --- * ---"
     # Create directory for each protein
     mkdir -p ../results/$basename
     cd ../results/$basename
 
+    echo "\n"
+    echo "--- * --- GROMACS format and create topology --- * ---\n"
+    echo "\n"
+
     # Convert PDB to GROMACS format and create topology
     $gmx pdb2gmx -f ../../pdbs/$basename.pdb -o processed.gro -p topol.top -ignh -water $gmxwatermodel -ff $gmxforcefield
-    echo "--- * --- GROMACS format and create topology --- * ---"
-
+    
+    echo "\n"
+    echo "--- * --- * --- Solvate + box --- * --- * ---\n"
+    echo "\n"
 
     # Define the box and solvate
     $gmx editconf -f processed.gro -o boxed.gro -c -d 1.0 -bt cubic
     $gmx solvate -cp boxed.gro -cs spc216.gro -o solvated.gro -p topol.top
-    echo "--- * --- Solvate + box --- * ---"
+
+    echo "\n"
+    echo "--- * --- * --- Add Ions --- * --- * ---\n"
+    echo "\n"
 
     # Add ions
     $gmx grompp -f ../../mdp_files/ions.mdp -c solvated.gro -p topol.top -o ions.tpr
@@ -32,13 +40,28 @@ for pdbfile in ../pdbs/*.pdb; do
     # 13 = SOL for genion - change if you dont want that...
     echo 13 | $gmx genion -s ions.tpr -o solv_ions.gro -p topol.top -pname NA -nname CL -neutral #-conc 0.15
 
+    echo "\n"
+    echo "--- * --- * --- Energy Minimisation --- * --- * ---\n"
+    echo "\n"
+
     # Energy minimization
     $gmx grompp -f ../../mdp_files/minim.mdp -c solv_ions.gro -p topol.top -o em.tpr
     $gmx mdrun -v -deffnm em
 
     # Equilibration steps: NVT then NPT
+
+    echo "\n"
+    echo "--- * --- * --- Equilibrate --- * --- * ---\n"
+    echo "\n \n"
+    echo " >> NVT"
+    echo "\n \n"
+
     $gmx grompp -f ../../mdp_files/nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
     $gmx mdrun -deffnm nvt
+    
+    echo "\n"
+    echo " >> NPT"
+    echo "\n \n"
 
     $gmx grompp -f ../../mdp_files/npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
     $gmx mdrun -deffnm npt
